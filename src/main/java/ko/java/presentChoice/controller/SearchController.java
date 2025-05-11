b76549546194ad837a02ee93b8cb2016c7f25377
heedayday <heeday3001@gmail.com>
2025-05-11 18:58:49 +0900

    http://localhost:8080/search/main/ 로 index.jsp 접속이 가능하도록 구현

---
+++ src/main/java/ko/java/presentChoice/controller/SearchController.java
@@ -0,0 +1,43 @@
+package ko.java.presentChoice.controller;
+
+import ko.java.presentChoice.service.SearchService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/main")
+    public ModelAndView MainSearch(HttpServletRequest request, HttpSession session) {
+        //Session에 데이터 셋팅
+        String setData = StringUtils.defaultIfEmpty(request.getParameter("setData"),"SETDATA");
+        session.setAttribute("SetData", setData);
+
+        //화면 페이지 이동 부분
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");       // templates 또는 WEB-INF/views 아래의 JSP, HTML 등
+        mav.addObject("result", "검색완료");  // 모델 데이터 추가
+
+        // 검색 관련 데이터 처리 비즈니스
+        List<String> result = this.searchService.searchResultList();
+        mav.addObject("result", result);
+        return mav;
+    }
+
+
+
+}
